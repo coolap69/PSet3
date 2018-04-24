8a830c787a13febd64c77290e6a5a6a28fecd730
Louis Pazos <coolap69@aol.com>
2018-04-24 01:47:21 +0000

    is_rest

--- music/helpers.c
+++ music/helpers.c
@@ -1,23 +1,52 @@
 // Helper functions for music
 
 #include <cs50.h>
+#include <string.h>
+
 
 #include "helpers.h"
 
 // Converts a fraction formatted as X/Y to eighths
 int duration(string fraction)
 {
-    // TODO
+    // TODO - implement duration
+    //take in a fraction as a string
+    //examples: "1/8", "1/4", "3/8"
+
+    //assume numnerator and denominator are each one digit
+
+    //assume that the fraction works out to a
+    //whole number of eighth notes
+
+
+
 }
 
 // Calculates frequency (in Hz) of a note
 int frequency(string note)
 {
-    // TODO
+    // TODO - implement frequency
 }
 
 // Determines whether a string represents a rest
 bool is_rest(string s)
 {
-    // TODO
+    // TODO - implement is_rest
+    // if s represents a rest, returns true;
+    //otherwise, returns false
+string empty = "";
+
+int compare = strcmp(s, empty);
+
+    // if (s == "\0")
+     if (compare == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+    //remember how we represent rest using our notation
 }
