a6058c8cf92eb1f445317efe53398bfe0366e398
Louis Pazos <coolap69@aol.com>
2018-04-25 02:59:02 +0000

    working on music

--- music/helpers.c
+++ music/helpers.c
@@ -8,7 +8,7 @@
 
 // Converts a fraction formatted as X/Y to eighths
 int duration(string fraction)
-{
+
     // TODO - implement duration
     //take in a fraction as a string
     //examples: "1/8", "1/4", "3/8"
@@ -18,9 +18,19 @@ int duration(string fraction)
     //assume that the fraction works out to a
     //whole number of eighth notes
 
+    int dur = GetInt(string fraction);
 
-
-}
+    switch (dur[2])
+    {
+    case 1:
+        printf(num * 4"\n");
+        break;
+    case 2:
+        printf(num * 2"\n");
+        break;
+    default:
+        printf(num"\n");
+    }
 
 // Calculates frequency (in Hz) of a note
 int frequency(string note)
